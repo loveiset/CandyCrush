ef519aa93ce146c763a788024a5a0835b26ce195
loveiset <loveiset@qq.com>
2014-08-14 17:20:37 +0800

    08-14

---
+++ Assets/Scripts/Candy.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Candy : MonoBehaviour {
+
+    public int rowIndex = 0;
+    public int columIndex = 0;
+
+    public float xOffset = 0f;
+    public float yOffset = 0f;
+    public int candyType;
+
+    public GameObject[] candyBG;
+    private GameObject candy;
+
+    public GameController gameController;
+
+	// Use this for initialization
+	void Start () 
+    {
+
+	}
+
+    void OnMouseDown()
+    {
+        gameController.Select(this);
+    }
+
+    private void AddRandomBG()
+    {
+        if (candy == null)
+        {
+            candyType = Random.Range(0, candyBG.Length);
+            candy = Instantiate(candyBG[candyType]) as GameObject;
+            candy.transform.parent = this.transform;
+        }
+    }
+
+	// Update is called once per frame
+	public void UpdatePosition () 
+    {
+        AddRandomBG();
+        transform.position = new Vector3(columIndex + xOffset, rowIndex + yOffset, 0f);
+	}
+
+    public void Dispose()
+    {
+        gameController = null;
+        Destroy(candy.gameObject);
+
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public Candy candy;
+
+    public int rowNum = 0;
+    public int columNum = 0;
+
+    public GameObject gameController;
+
+    private Candy lastSelectedCandy = null;
+    private ArrayList candyArray;
+
+	// Use this for initialization
+	void Start () 
+    {
+        candyArray = new ArrayList();
+        for (int rowIndex = 0; rowIndex < rowNum; rowIndex++)
+        {
+            ArrayList tempArray = new ArrayList();
+            for (int columIndex = 0; columIndex < columNum; columIndex++)
+            {
+                Candy c = Instantiate(candy) as Candy;
+                c.transform.parent = this.transform;
+                c.columIndex = columIndex;
+                c.rowIndex = rowIndex;
+                c.gameController = this;
+                c.UpdatePosition();
+                tempArray.Add(c);
+            }
+            candyArray.Add(tempArray);
+        }
+	}
+
+    private Candy GetCandy(int rowIndex, int columIndex)
+    {
+        ArrayList temp = candyArray[rowIndex] as ArrayList;
+        return temp[columIndex] as Candy;
+    }
+
+    private void SetCandy(int rowIndex, int columIndex, Candy candy)
+    {
+        ArrayList temp = candyArray[rowIndex] as ArrayList;
+        temp[columIndex] = candy;
+    }
+
+    public void Select(Candy candy)
+    {
+        Remove(candy);
+        return;
+        if (lastSelectedCandy == null)
+        {
+            lastSelectedCandy = candy;
+            return;
+        }
+        else
+        {
+            Exchange(lastSelectedCandy, candy);
+            lastSelectedCandy = null;
+        }
+    }
+
+    private void Exchange(Candy lastSelected, Candy nowCandy)
+    {
+        int rowIndex = lastSelected.rowIndex;
+        lastSelected.rowIndex = nowCandy.rowIndex;
+        nowCandy.rowIndex = rowIndex;
+
+        int columIndex = lastSelected.columIndex;
+        lastSelected.columIndex = nowCandy.columIndex;
+        nowCandy.columIndex = columIndex;
+
+        lastSelected.UpdatePosition();
+        nowCandy.UpdatePosition();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void Remove(Candy candy)
+    {
+        candy.Dispose();
+        for (int rowIndex = candy.rowIndex + 1; rowIndex < rowNum; rowIndex++)
+        {
+            Candy candyToMove = GetCandy(rowIndex, candy.columIndex);
+            candyToMove.rowIndex--;
+            candyToMove.UpdatePosition();
+
+            SetCandy(rowIndex - 1, candy.columIndex, candyToMove);
+        }
+    }
+}
